1959e8108952d6027c9aa04d7307adae227d2947
lintosunny <lintosunny111@gmail.com>
2026-03-24 22:21:08 +0530

    started week 5

--- programming_data_structures_and_algorithms_using_python/week_01.py
+++ programming_data_structures_and_algorithms_using_python/week_01.py
@@ -1,3 +1,19 @@
+# In algorithm we must get the outcome after a finite number of steps
+
+# Algorithm: how to systamatically perform a task
+#  * write down as sequence of steps
+#  * programming language describes the steps
+
+# Program is a sequence of steps
+
+# some steps are repeated
+#  * do the same thing for each item in a list
+#     * for - for fixed number
+#     * while - don't know advance how many times we will repeat
+
+# some steps are executed conditionally
+#  * do something if a value meets some requirement
+
 # way 1: find the gratest common divisor
 def gcd(m: int, n: int) -> int:
 
