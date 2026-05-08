53780e3a02223cf3fb751f02b54c6aeea04c3a4d
lintosunny <lintosunny111@gmail.com>
2026-05-08 10:20:01 +0530

    started BST

---
+++ python_dsa_and_leetcode_questions/4_tree/0_tree.py
@@ -0,0 +1,17 @@
+# A Tree is a hierarchical, non-linear data structure made up of nodes connected by edges, with a single root node.
+# Node: Basic element of a tree
+# Root: Topmost node
+# Parent Node: A node that has one or more children
+# Child Node: A node derived from another node
+# Leaf Node: Node with no children
+# Subtree: Tree formed by a node and its descendants
+# Depth: Distance from root to a node
+# Height: Longest path from root to a leaf
+# Time complexity: O(n) (skewed tree)
+
+# Full Binary Tree: Every node has 0 or 2 children. No node has exactly 1 child.
+# Perfect Binary Tree: All nodes have 2 children (except leaves). All leaf nodes are at the same level. Total nodes = 2^h - 1
+# Complete Binary Tree: All levels are filled except possibly the last. Last level is filled from left to right.
+
+# Binary Search Tree
+# Values smaller than the root are stored on the left and larger values are stored on the right.
