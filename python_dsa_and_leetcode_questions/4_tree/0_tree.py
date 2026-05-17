7460ab5533f5f569509c5b63a7cf295c4e56a241
lintosunny <lintosunny111@gmail.com>
2026-05-17 20:06:45 +0530

    binary search tree basics

--- python_dsa_and_leetcode_questions/4_tree/0_tree.py
+++ python_dsa_and_leetcode_questions/4_tree/0_tree.py
@@ -15,3 +15,60 @@
 
 # Binary Search Tree
 # Values smaller than the root are stored on the left and larger values are stored on the right.
+
+class Node:
+    def __init__(self, value):
+        self.value = value 
+        self.right = None 
+        self.left = None 
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None 
+
+    def insert(self, value):
+        new_node = Node(value)
+        if self.root is None:
+            self.root = new_node 
+            return True
+
+        temp = self.root
+        while True:
+            if temp.value == new_node.value:
+                return False 
+
+            if temp.value < new_node.value:
+                if temp.right is None:
+                    temp.right = new_node
+                    return True
+                temp = temp.right
+            else:
+                if temp.left is None:
+                    temp.left = new_node
+                    return True
+                temp = temp.left
+
+    def contains(self, value):
+        if self.root is None:
+            return False 
+        temp = self.root 
+        while temp is not None:
+            if temp.value > value:
+                temp = temp.left
+            elif temp.value < value:
+                temp = temp.right
+            else:
+                return True
+        return False
+
+
+
+my_tree = BinarySearchTree()
+print(my_tree.root)
+my_tree.insert(10)
+print(my_tree.root.value)
+my_tree.insert(200)
+print(my_tree.root.right.value)
+my_tree.insert(50)
+print(my_tree.root.right.left.value)
+print(my_tree.contains(200))
