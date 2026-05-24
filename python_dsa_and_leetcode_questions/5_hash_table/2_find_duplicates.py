b711703f23dc8bc827bdb09578752affedd1e90f
lintosunny <lintosunny111@gmail.com>
2026-05-24 08:43:24 +0530

    started hash map questions

---
+++ python_dsa_and_leetcode_questions/5_hash_table/2_find_duplicates.py
@@ -0,0 +1,49 @@
+# Problem: 
+# Given an array of integers nums, find all the duplicates in the array using a hash table (dictionary).
+
+# Input: 
+# A list of integers nums.
+
+# Output: 
+# A list of integers representing the numbers in the input array nums that appear more than once. 
+# If no duplicates are found in the input array, return an empty list [].
+
+
+def find_duplicates(nums):
+    num_counts = {}
+
+    for num in nums:
+        num_counts[num] = num_counts.get(num, 0) + 1 
+    
+    duplicates = []
+    
+    for num, count in num_counts.items():
+        if count > 1:
+            duplicates.append(num)
+            
+    return duplicates
+            
+    
+
+print ( find_duplicates([1, 2, 3, 4, 5]) )
+print ( find_duplicates([1, 1, 2, 2, 3]) )
+print ( find_duplicates([1, 1, 1, 1, 1]) )
+print ( find_duplicates([1, 2, 3, 3, 3, 4, 4, 5]) )
+print ( find_duplicates([1, 1, 2, 2, 2, 3, 3, 3, 3]) )
+print ( find_duplicates([1, 1, 1, 2, 2, 2, 3, 3, 3, 3]) )
+print ( find_duplicates([]) )
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    []
+    [1, 2]
+    [1]
+    [3, 4]
+    [1, 2, 3]
+    [1, 2, 3]
+    []
+
+"""
---
+++ python_dsa_and_leetcode_questions/5_hash_table/4_group_anagrams.py
@@ -0,0 +1,11 @@
+# You have been given an array of strings, where each string may contain only lowercase English letters. 
+# You need to write a function group_anagrams(strings) that groups the anagrams in the array together using a hash table (dictionary). 
+# The function should return a list of lists, where each inner list contains a group of anagrams.
+
+# For example, if the input array is ["eat", "tea", "tan", "ate", "nat", "bat"], the function should return [["eat","tea","ate"],["tan","nat"],["bat"]] 
+# because the first three strings are anagrams of each other, the next two strings are anagrams of each other, and the last string has no anagrams in the input array.
+
+# You need to implement the group_anagrams(strings) function and return a list of lists, where each inner list contains a group of anagrams according to the above requirements.
+
+t = "adc"
+print("".join(sorted(t)))
