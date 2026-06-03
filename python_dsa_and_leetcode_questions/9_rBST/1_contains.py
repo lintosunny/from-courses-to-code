898cc108e3625063ca55a103a1009e9cf3e0ddef
lintosunny <lintosunny111@gmail.com>
2026-06-03 11:23:25 +0530

    started rBST

---
+++ python_dsa_and_leetcode_questions/9_rBST/1_contains.py
@@ -0,0 +1,58 @@
+class Node:
+    def __init__(self, value):
+        self.value = value 
+        self.right = None 
+        self.left = None 
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None 
+
+    def insert(self, value):
+        new_node = Node(value)
+        if self.root is None:
+            self.root = new_node 
+            return True
+
+        temp = self.root
+        while True:
+            if temp.value == new_node.value:
+                return False 
+
+            if temp.value < new_node.value:
+                if temp.right is None:
+                    temp.right = new_node
+                    return True
+                temp = temp.right
+            else:
+                if temp.left is None:
+                    temp.left = new_node
+                    return True
+                temp = temp.left
+
+    def __r_contains(self, current_node, value):
+        if current_node == None:
+            return False 
+        if value == current_node.value:
+            return True 
+        if value < current_node.value:
+            return self.__r_contains(current_node.left, value)
+        if value > current_node.value:
+            return self.__r_contains(current_node.right, value)
+        
+    def r_contains(self, value):
+        return self.__r_contains(self.root, value)
+    
+
+my_tree = BinarySearchTree()
+my_tree.insert(23)
+my_tree.insert(3)
+my_tree.insert(31)
+my_tree.insert(13)
+my_tree.insert(56)
+my_tree.insert(89)
+my_tree.insert(67)
+my_tree.insert(98)
+
+print(f"BST contains 31: {my_tree.r_contains(31)}")
+print(f"BST contains 2: {my_tree.r_contains(2)}")
