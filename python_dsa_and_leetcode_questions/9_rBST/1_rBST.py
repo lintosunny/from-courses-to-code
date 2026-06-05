cc0b43f18566e2ce7b052f297944fdde180fc8b9
lintosunny <lintosunny111@gmail.com>
2026-06-05 11:20:49 +0530

    rBST interview questions

--- python_dsa_and_leetcode_questions/9_rBST/1_rBST.py
+++ python_dsa_and_leetcode_questions/9_rBST/1_rBST.py
@@ -57,6 +57,34 @@ class BinarySearchTree:
             self.root = Node(value)
         self.__r_insert(self.root, value)
 
+    def min_value(self, current_node):
+        while current_node.left is not None:
+            current_node = current_node.left
+        return current_node.value
+    
+    def __delete_node(self, current_node, value):
+        if current_node == None:
+            return None
+        if value < current_node.value:
+            current_node.left = self.__delete_node(current_node.left, value)
+        elif value > current_node.value:
+            current_node.right = self.__delete_node(current_node.right, value)
+        else:
+            if current_node.left == None and current_node.right == None:
+                return None 
+            elif current_node.left == None:
+                current_node = current_node.right 
+            elif current_node.right == None:
+                current_node = current_node.left
+            else:
+                sub_tree_min = self.min_value(current_node.right)
+                current_node.value = sub_tree_min
+                current_node.right = self.__delete_node(current_node.right, sub_tree_min)
+        return current_node
+
+    def delete_node(self, value):
+        self.root = self.__delete_node(self.root, value)
+
 my_tree = BinarySearchTree()
 my_tree.r_insert(23)
 my_tree.r_insert(3)
@@ -68,3 +96,5 @@ print(f"Root -> Right: {my_tree.root.right.value}")
 
 print(f"BST contains 31: {my_tree.r_contains(31)}")
 print(f"BST contains 2: {my_tree.r_contains(2)}")
+
+print(f"min value: {my_tree.min_value(my_tree.root)}")
